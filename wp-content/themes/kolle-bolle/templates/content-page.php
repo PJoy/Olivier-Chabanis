7d04e91d8e3f936801ef971f717ca7e3c174e2ee
Pierre Portejoie <portejoiepierre@gmail.com>
2017-08-23 16:01:51 +0200

    [pages] main content + draft styling

--- wp-content/themes/kolle-bolle/templates/content-page.php
+++ wp-content/themes/kolle-bolle/templates/content-page.php
@@ -1,2 +1,18 @@
-<?php the_content(); ?>
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+<?php $blocks = get_field("contenu"); ?>
+
+<div class="page-content col-12 col-md-10 m-auto row">
+	<?php foreach ($blocks as $block) {
+		$bg = $block["image"]["url"];
+		$link = ($block["lien_interne"] != '')
+			? $block["lien_interne"]
+			: $block["lien_externe"];
+		?>
+		<div class="col-md-4 text-center" <?php if ($bg != '') echo 'style="background: url('.$bg.')"'?>>
+			<?php if ($link != '') echo '<a href="'.$link.'">'; ?>
+			<p>
+				<?php echo $block["texte"]?>
+			</p>
+			<?php if ($link != '') echo '</a>'; ?>
+		</div>
+	<?php } ?>
+</div>
