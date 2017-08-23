ad791b15f84ea4852c4d6a2ec94afba560e7106f
Pierre Portejoie <portejoiepierre@gmail.com>
2017-08-23 17:13:32 +0200

    [pages] better block styming

--- wp-content/themes/kolle-bolle/templates/content-page.php
+++ wp-content/themes/kolle-bolle/templates/content-page.php
@@ -6,13 +6,19 @@
 		$link = ($block["lien_interne"] != '')
 			? $block["lien_interne"]
 			: $block["lien_externe"];
+		$class = ($bg != '')
+			? 'image-block'
+			: (($link != '')
+				? 'color-block'
+				: 'simple-block' );
 		?>
-		<div class="col-md-4 text-center" <?php if ($bg != '') echo 'style="background: url('.$bg.')"'?>>
-			<?php if ($link != '') echo '<a href="'.$link.'">'; ?>
-			<p>
+		<div class="outer-content-block col-lg-6 col-xl-4 text-center">
+			<div class="content-block <?php echo $class ?>" <?php if ($bg != '') echo 'style="background: url('.$bg.')"'?>>
+				<?php if ($link != '') echo '<a href="'.$link.'">'; ?>
 				<?php echo $block["texte"]?>
-			</p>
-			<?php if ($link != '') echo '</a>'; ?>
+				<?php if ($link != '') echo '</a>'; ?>
+				<?php if ($bg != '') echo '<div class="bg-color"></div>' ?>
+			</div>
 		</div>
 	<?php } ?>
 </div>
