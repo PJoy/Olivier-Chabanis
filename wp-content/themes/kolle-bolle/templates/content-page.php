17ba2a77a36640575582fbf7887d7d3f507ee768
Pierre Portejoie <portejoiepierre@gmail.com>
2017-08-24 12:52:57 +0200

    [pages] hover on blocks

--- wp-content/themes/kolle-bolle/templates/content-page.php
+++ wp-content/themes/kolle-bolle/templates/content-page.php
@@ -11,9 +11,14 @@
 			: (($link != '')
 				? 'color-block'
 				: 'simple-block' );
+		$font = ($block["type"] == "Lien")
+            ? 'bigger-lighter'
+            : ($block["type"] == "Texte court"
+                ? 'big-regular'
+                : 'small-light' );
 		?>
 		<div class="outer-content-block col-lg-6 col-xl-4">
-			<div class="content-block <?php echo $class ?>" <?php if ($bg != '') echo 'style="background: url('.$bg.')"'?>>
+			<div class="content-block <?php echo $class.' font-'.$font; ?>" <?php if ($bg != '') echo 'style="background: url('.$bg.')"'?>>
 				<?php if ($link != '') echo '<a href="'.$link.'">'; ?>
 				<?php echo $block["texte"]?>
 				<?php if ($link != '') echo '</a>'; ?>
