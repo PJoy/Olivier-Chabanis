21b9bc7350bd0bdeaee33ab4670ccd59d20e410d
Pierre Portejoie <portejoiepierre@gmail.com>
2017-08-26 19:02:21 +0200

    [header] new content

--- wp-content/themes/kolle-bolle/templates/header.php
+++ wp-content/themes/kolle-bolle/templates/header.php
@@ -1,11 +1,14 @@
 <header class="banner">
     <div class="container">
+            <a href="<?php get_home_url(); ?>">
+                <img class="logo" src="<?php echo get_template_directory_uri()."/assets/images/header-logo.png"; ?>">
+            </a>
         <nav class="nav-primary text-center d-none d-md-block">
 			<?php
-            $menu_elems = wp_get_nav_menu_items('Main');
-            foreach ($menu_elems as $menu_elem) {
-                echo '<a href="'.$menu_elem->url.'">'.$menu_elem->title.'</a>';
-            }
+			$menu_elems = wp_get_nav_menu_items('Main');
+			foreach ($menu_elems as $menu_elem) {
+				echo '<a class="menu-elem" href="'.$menu_elem->url.'"><span>'.$menu_elem->title.'</span></a>';
+			}
 			?>
         </nav>
     </div>
