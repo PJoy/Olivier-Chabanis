4ad0501ec75ba822aa3f785277376da11a3183bb
Pierre Portejoie <portejoiepierre@gmail.com>
2017-09-07 14:16:24 +0200

    draft mobile menu

--- wp-content/themes/kolle-bolle/templates/header.php
+++ wp-content/themes/kolle-bolle/templates/header.php
@@ -1,11 +1,33 @@
 <header class="banner">
-	<div class="container">
+	<div class="container desktop">
 		<a href="<?php get_home_url(); ?>">
 			<div class="logo" style="background:
 				url('<?php echo get_template_directory_uri()."/assets/images/header-logo.png"; ?>') no-repeat">
 
 			</div>
 		</a>
+		<nav class="nav-primary">
+			<?php
+			$menu_elems = wp_get_nav_menu_items('Main');
+			foreach ($menu_elems as $menu_elem) {
+				echo '<a class="menu-elem" href="'.$menu_elem->url.'"><span>'.$menu_elem->title.'</span></a>';
+			}
+			?>
+		</nav>
+	</div>
+
+	<div class="container mobile">
+		<a href="<?php get_home_url(); ?>">
+			<div class="logo" style="background:
+				url('<?php echo get_template_directory_uri()."/assets/images/header-logo.png"; ?>') no-repeat">
+			</div>
+		</a>
+
+		<div class="menu-btn" style="background:
+			url('<?php echo get_template_directory_uri()."/assets/images/ham-menu.png"; ?>') no-repeat"
+		     onclick="jQuery('.container.mobile .nav-primary').slideToggle()">
+		</div>
+
 		<nav class="nav-primary">
 			<?php
 			$menu_elems = wp_get_nav_menu_items('Main');
