96f9bba94344d072bae2fca8a40b71031147c59d
Pierre Portejoie <portejoiepierre@gmail.com>
2017-09-07 17:11:34 +0200

    [contact] draft styling

--- wp-content/themes/kolle-bolle/templates/pages/contact.php
+++ wp-content/themes/kolle-bolle/templates/pages/contact.php
@@ -1,35 +1,37 @@
 <div class="contact-page">
-	<hr>
-	<div class="contact-info">
-		<p>
-		<b>—IF—</b>
-		31 bis, quai Fulchiron - 69005 LYON
-		T.0478065058 — F.0478065231
-		contact@ifcontemporain.com
-		Laurence / 06 15 05 33 22 — Catherine / 06 07 67 06 59
-		</p>
+	<div class="contact-page-inner">
+		<hr id="hr1">
+		<div class="contact-info">
+			<p>
+				<b>—IF—</b><br>
+				31 bis, quai Fulchiron - 69005 LYON<br>
+				T.0478065058 — F.0478065231<br>
+				contact@ifcontemporain.com<br>
+				Laurence / 06 15 05 33 22 — Catherine / 06 07 67 06 59<br>
+			</p>
+		</div>
+		<hr id="hr2">
+
+		<div class="contact-form">
+			<form>
+				<div class="form-group">
+
+					<label for="inputEmail"><b>-</b> Votre adresse mail <b>-</b></label>
+					<input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="">
+				</div>
+
+				<div class="form-group">
+					<label for="inputTextarea"><b>-</b> Votre message <b>-</b></label>
+					<textarea class="form-control" id="inputTextarea" rows="3"></textarea>
+				</div>
+
+				<button type="submit" class="btn btn-primary">Envoyer</button>
+
+			</form>
+		</div>
+		<hr id="hr3">
+
+		<p><b>-</b> Plan d'accès <b>-</b></p>
+		<div style="background: url('<?php echo get_template_directory_uri()."/assets/images/plan.png"?>')"
 	</div>
-	<hr>
-
-	<div class="contact-form">
-		<form>
-			<div class="form-group">
-
-			<label for="inputEmail">- Votre adresse mail -</label>
-			<input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="">
-</div>
-
-			<div class="form-group">
-				<label for="inputTextarea">- Votre message -</label>
-				<textarea class="form-control" id="inputTextarea" rows="3"></textarea>
-			</div>
-
-			<button type="submit" class="btn btn-primary">Envoyer</button>
-
-		</form>
-	</div>
-	<hr>
-
-	<p>- Plan d'accès -</p>
-	<div style="background: url('<?php echo get_template_directory_uri()."/assets/images/plan.png"?>')"
 </div>
