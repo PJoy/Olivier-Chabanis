1f0f0cc63dabe0daead5ce2defb90c34b8946ac4
Pierre Portejoie <portejoiepierre@gmail.com>
2017-09-07 15:25:53 +0200

    remove page header when no page header

--- wp-content/themes/kolle-bolle/templates/page-header.php
+++ wp-content/themes/kolle-bolle/templates/page-header.php
@@ -1,5 +1,9 @@
 <?php use Roots\Sage\Titles; ?>
 
-<div class="page-header" style="background: url('<?php echo the_post_thumbnail_url() ?>')">
-  <h1><?= get_field("sous-titre") ?></h1>
-</div>
+<?php
+$thumb = get_the_post_thumbnail_url();
+if ($thumb != '') {?>
+	<div class="page-header" style="background: url('<?php echo get_the_post_thumbnail_url() ?>')">
+		<h1><?= get_field("sous-titre") ?></h1>
+	</div>
+<?php } ?>
