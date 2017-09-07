65189178ba001ea39c02bbbdd4deaed51b4b057d
Pierre Portejoie <portejoiepierre@gmail.com>
2017-09-07 15:52:05 +0200

    [contact] add content

--- wp-content/themes/kolle-bolle/templates/content-page.php
+++ wp-content/themes/kolle-bolle/templates/content-page.php
@@ -1,42 +1,48 @@
-<?php $blocks = get_field("blocs"); ?>
+<?php $blocks = get_field("blocs");
+if ($blocks != false) {
+	?>
 
 	<div class="content-blocks-wrapper grid">
-		<?php foreach ($blocks as $key=>$block){
-			$hasImage = (strcmp($block["type"],"bloc image") == 0);
-			$hasText = (strcmp($block["texte"],'') != 0);
-			$hasLink = (strcmp($block["lien"],'') != 0);
+		<?php foreach ( $blocks as $key => $block ) {
+			$hasImage = ( strcmp( $block["type"], "bloc image" ) == 0 );
+			$hasText  = ( strcmp( $block["texte"], '' ) != 0 );
+			$hasLink  = ( strcmp( $block["lien"], '' ) != 0 );
 
 			$classes = '';
-			$classes .= ($hasImage ? 'image-block ' : 'text-block ');
-			$classes .= ($hasText ? seoUrl($block["gabarit"]).' ' : '');
+			$classes .= ( $hasImage ? 'image-block ' : 'text-block ' );
+			$classes .= ( $hasText ? seoUrl( $block["gabarit"] ) . ' ' : '' );
 
-			$classes .= 'block-x-3-'.$block["taille_x_3"].' ';
-			$classes .= 'block-y-3-'.$block["taille_y_3"].' ';
-			$classes .= 'block-x-2-'.$block["taille_x_2"].' ';
-			$classes .= 'block-y-2-'.$block["taille_y_2"].' ';
-			$classes .= 'block-x-1-'.$block["taille_x_1"].' ';
-			$classes .= 'block-y-1-'.$block["taille_y_1"].' ';
+			$classes .= 'block-x-3-' . $block["taille_x_3"] . ' ';
+			$classes .= 'block-y-3-' . $block["taille_y_3"] . ' ';
+			$classes .= 'block-x-2-' . $block["taille_x_2"] . ' ';
+			$classes .= 'block-y-2-' . $block["taille_y_2"] . ' ';
+			$classes .= 'block-x-1-' . $block["taille_x_1"] . ' ';
+			$classes .= 'block-y-1-' . $block["taille_y_1"] . ' ';
 
 			$data = '';
-			$data .= 'data-pos3 = "'.$block["position_3"].'" ';
-			$data .= 'data-pos2 = "'.$block["position_2"].'" ';
-			$data .= 'data-pos1 = "'.$block["position_1"].'" ';
+			$data .= 'data-pos3 = "' . $block["position_3"] . '" ';
+			$data .= 'data-pos2 = "' . $block["position_2"] . '" ';
+			$data .= 'data-pos1 = "' . $block["position_1"] . '" ';
 
 			$doInvert = $block["inversion"];
 
-			$outerClasses = "grid-item--width".$block["taille_x_3"].' ';
-			if ($doInvert) $outerClasses .= "invert invert-".$block["invert_index"];
+			$outerClasses = "grid-item--width" . $block["taille_x_3"] . ' ';
+			if ( $doInvert ) {
+				$outerClasses .= "invert invert-" . $block["invert_index"];
+			}
 
 			?>
 			<div class="content-block-outer grid-item <?php echo $outerClasses; ?>" <?php echo $data; ?>>
-				<?php if ($hasLink) {
+				<?php if ( $hasLink ) {
 					echo '<a href="">';
 				} ?>
-				<div id="<?php echo 'block-'.$key; ?>" class="content-block <?php echo $classes; ?>">
-					<?php if ($hasImage) echo '<div class="background-image" style="background: url('.$block["image"].')"></div>'?>
-					<?php if ($hasText) echo $block["texte"] ?>
+				<div id="<?php echo 'block-' . $key; ?>" class="content-block <?php echo $classes; ?>">
+					<?php if ( $hasImage )
+						echo '<div class="background-image" style="background: url(' . $block["image"] . ')"></div>' ?>
+					<?php if ( $hasText )
+						echo $block["texte"] ?>
 				</div>
-				<?php if ($hasLink) {
+				<?php if ( $hasLink ) {
 					echo '<div class="color-hover"></div>';
 					echo '</a>';
 				} ?>
@@ -94,6 +100,10 @@
 		}
 	</script>
 
-<?php if (is_front_page()) {
+	<?php
+}
+if (is_front_page()) {
 	include("pages/home.php");
+} elseif ($post->post_name == "nous-contacter") {
+	include ("pages/contact.php");
 }
