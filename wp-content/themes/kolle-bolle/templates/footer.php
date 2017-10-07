934efc6df1477c0ec12354c79f6afc3d408601ce
Pierre Portejoie <portejoiepierre@gmail.com>
2017-10-07 17:16:13 +0200

    add footer

--- wp-content/themes/kolle-bolle/templates/footer.php
+++ wp-content/themes/kolle-bolle/templates/footer.php
@@ -1,5 +1,11 @@
 <footer class="content-info">
 	<div class="container">
-		<?php dynamic_sidebar('sidebar-footer'); ?>
+		<a href="https://www.facebook.com/IFcontemporain/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+		<a href="https://www.instagram.com/if_contemporain/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+		<a href=""><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+
+		<br>
+
+		© <a href="/">IF Contemporain</a>  - Conception graphique <a href="kollebolle.com">Kolle Bolle</a> - <a href="portejoie.fr">Développeur Web</a>
 	</div>
 </footer>
