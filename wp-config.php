1524fb3bb15305f52017e82f9ec77ba77403e0b4
Pierre Portejoie <portejoiepierre@gmail.com>
2017-12-30 10:49:04 +0100

    init from IF to OLIV

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'ifcontemporain');
+define('DB_NAME', 'oliv');
 
 /** MySQL database username */
 define('DB_USER', 'root');
