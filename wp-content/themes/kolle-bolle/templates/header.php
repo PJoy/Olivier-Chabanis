30530899ccbbc81d593b56e9b26bbda35b393de2
Pierre Portejoie <portejoiepierre@gmail.com>
2018-01-31 12:00:02 +0100

    add gototop button

--- wp-content/themes/kolle-bolle/templates/header.php
+++ wp-content/themes/kolle-bolle/templates/header.php
@@ -73,3 +73,47 @@
 		jQuery(this).addClass('active');
 	})
 </script>
+
+<script src="https://use.fontawesome.com/1f0ceada91.js"></script>
+
+<button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fa fa-chevron-up" aria-hidden="true"></i>
+</button>
+
+<style>
+	#myBtn {
+		display: none; /* Hidden by default */
+		position: fixed; /* Fixed/sticky position */
+		bottom: 20px; /* Place the button at the bottom of the page */
+		right: 30px; /* Place the button 30px from the right */
+		z-index: 99; /* Make sure it does not overlap */
+		border: none; /* Remove borders */
+		outline: none; /* Remove outline */
+		background-color: blue; /* Set a background color */
+		color: white; /* Text color */
+		cursor: pointer; /* Add a mouse pointer on hover */
+		padding: 3px 7px; /* Some padding */
+	}
+
+	#myBtn:hover {
+		background-color: #555; /* Add a dark-grey background on hover */
+	}
+</style>
+
+<script>
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+			document.getElementById("myBtn").style.display = "block";
+		} else {
+			document.getElementById("myBtn").style.display = "none";
+		}
+	}
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+		document.body.scrollTop = 0; // For Safari
+		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	}
+</script>
