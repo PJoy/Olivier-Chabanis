bd85edd2fe667ab31b98fac633b20557df5bd126
Pierre Portejoie <portejoiepierre@gmail.com>
2018-01-31 10:48:28 +0100

    add hr to footer

--- wp-content/themes/kolle-bolle/templates/footer.php
+++ wp-content/themes/kolle-bolle/templates/footer.php
@@ -1,5 +1,6 @@
 <footer class="content-info">
 	<div class="container">
+	<hr>
 		© <a href="/">Olivier Chabanis</a>  - Conception graphique <a href="http://kollebolle.com">Kolle Bolle</a> - <a href="http://portejoie.fr">Développeur Web</a> - <a href="/" rel="nofollow" target="_blank">Mentions légales</a>
 	</div>
 </footer>
